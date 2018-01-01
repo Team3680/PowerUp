dd6ed824200a564146256372b782421516e0102e
Michael York <me@myork.net>
2018-01-01 11:44:21 -0500

    Removed auto-generated comments. Added barebones drive subsystem.

---
+++ src/org/usfirst/frc/team3680/robot/commands/DriveTeleop.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team3680.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3680.robot.Robot;
+
+public class DriveTeleop extends Command {
+	public DriveTeleop() {
+		requires(Robot.driveSubsystem);
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void execute() {
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+	}
+
+	@Override
+	protected void interrupted() {
+	}
+}
---
+++ src/org/usfirst/frc/team3680/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3680.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveSubsystem extends Subsystem {
+
+	public void initDefaultCommand() {
+	}
+}
